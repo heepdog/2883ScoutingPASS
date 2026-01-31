5c5b0d6f1b0737418d6c3734539bbf7ab471d856
warbotics21848 <warbotics21848@gmail.com>
2026-01-31 16:55:58 -0600

    fixed save data function

--- resources/js/saveData.js
+++ resources/js/saveData.js
@@ -2,8 +2,15 @@ function saveLocalData(){
     console.log('save data called')
     mydata = getData(dataFormat);
     console.log(mydata)
-    laststore = localStorage.getItem("scoutData")
-    localStorage.setItem("scoutData", laststoreore + "\n" + mydata)
-    
+
+    if (Object.is(laststore,null)){
+        laststore = ""
+    }
+    else{
+        laststore = localStorage.getItem("scoutData")
+    }
+
+    localStorage.setItem("scoutData", laststore + "\n" + mydata)
+
 
 }
