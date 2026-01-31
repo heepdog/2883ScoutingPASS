66610106af64810cc0997146aec9ce739662fef9
warbotics21848 <warbotics21848@gmail.com>
2026-01-31 16:40:59 -0600

    save data to userspace

--- resources/js/saveData.js
+++ resources/js/saveData.js
@@ -2,4 +2,8 @@ function saveLocalData(){
     console.log('save data called')
     mydata = getData(dataFormat);
     console.log(mydata)
+    laststore = localStorage.getItem("scoutData")
+    localStorage.setItem("scoutData", laststoreore + "\n" + mydata)
+    
+
 }
